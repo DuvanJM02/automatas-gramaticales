aec84798908bbd91b717d5c0eaf41996f2a308ea
Webmaster Bavpublicidad <duvanlol02@gmail.com>
2022-11-24 12:13:53 -0500

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+// define formulario
 let frm = document.getElementById('form');
+
+let phrase = {
+    a: 'hola',
+    b: 'c',
+};
+
+let a = 'The+\s.+';
+let b = 'The cat'
+let c = 'I|You|He|She|It|We|They'
+let p = "j";
+let s = `${a}|${b}|${c}`; // sujeto
+let ExpReg = new RegExp("^("+s+")(\\s+"+p+")$");
+
+
+console.log(ExpReg.test('The j')); 
+
+console.log(phrase);
 
 frm.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -17,7 +35,15 @@ function validPhrase(sentence){
             let firstChar = sentence[0];
             let latChar = sentence[sentence.length - 1];
 
-            return /[A-Z]/.test(firstChar) && latChar == '.';
+            if(/[A-Z]/.test(firstChar) && latChar == '.'){
+
+
+                return 'Hola';
+            }else{
+                return false;
+            }
+
+            return ;
         }else{
             throw Error('La cadena debe contener al menos un carácter.');
         }
